17eae02e03c792620b1902136607a2571f2a8886
Subham Ball <123955835+subhamball@users.noreply.github.com>
2023-12-17 18:06:14 +0530

    Add files via upload

---
+++ user_input_print.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int number;
+    cout<<"enter a number :";
+    cin>>number;
+    cout<<"your enter number is : "<<number<<endl;
+
+    return 0;
+
+}
