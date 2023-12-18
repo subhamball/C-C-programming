7e55af8eca3cae1a2981a7a330ae5c560d272bba
Subham Ball <123955835+subhamball@users.noreply.github.com>
2023-12-18 16:02:41 +0530

    Add files via upload

---
+++ check_even_odd.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int num1;
+    cout<<"enter number"<<endl;
+    cin>>num1;
+    
+    if(num1%2==0){
+        cout<<"even number"<<endl;
+    }
+    else{
+        cout<<"odd number"<<endl;
+    }
+
+    return 0;
+
+}
