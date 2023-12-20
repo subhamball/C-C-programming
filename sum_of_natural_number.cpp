8440cf1fa53deedf565be80be695a20db8630206
Subham Ball <123955835+subhamball@users.noreply.github.com>
2023-12-20 11:39:26 +0530

    Add files via upload

---
+++ sum_of_natural_number.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num;
+    cout<<"enter a number: ";
+    cin>>num;
+
+    cout<<"sum of the number is: "<<(num*(num+1))/2<<endl;
+    
+    return 0;
+}
