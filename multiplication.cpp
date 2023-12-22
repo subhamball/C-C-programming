2b4d6e79bfe224978606c9d9d3dafc4d628a92b0
Subham Ball <123955835+subhamball@users.noreply.github.com>
2023-12-22 12:54:55 +0530

    Add files via upload

---
+++ multiplication.cpp
@@ -0,0 +1,18 @@
+//C++ Program to Generate Multiplication
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main(){
+    
+    int num1,num2;
+    cout<<"enter 1st number: ";
+    cin>>num1;
+    cout<<"enter 2nd number: ";
+    cin>>num2;
+
+    cout<<"multiplication of two number : "<<num1*num2<<endl;
+
+    return 0;
+
+}
