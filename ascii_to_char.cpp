91859fae6cc95daf0838527a90622d16e02c3254
Subham Ball <123955835+subhamball@users.noreply.github.com>
2023-12-23 22:55:56 +0530

    Add files via upload

---
+++ ascii_to_char.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main(){
+
+    char c;
+    cout<<"enter a Character: ";
+    cin>>c;
+
+    cout<<"ASCII Value of this character is: "<<int(c)<<endl;
+
+    return 0;
+
+}
