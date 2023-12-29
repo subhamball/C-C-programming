700d0b5a57544cf998c6363010f463e23543e633
Subham Ball <123955835+subhamball@users.noreply.github.com>
2023-12-29 22:53:11 +0530

    Add files via upload

---
+++ smallest_element_in_array.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"enter length of the array: ";
+    cin>>n;
+    int a[n];
+    cout<<"enter element of array : ";
+
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+
+    for(int i=1;i<n;i++){
+        if(a[0]>a[i]){
+            a[0]=a[i];
+        }
+    }
+    cout<<"smallest element is : "<<a[0]<<endl;
+
+    return 0;
+
+}
