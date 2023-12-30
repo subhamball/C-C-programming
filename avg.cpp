b31e8ce838cb6291989958d8c396102c29502907
Subham Ball <123955835+subhamball@users.noreply.github.com>
2023-12-30 15:22:14 +0530

    Add files via upload

---
+++ avg.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num1,num2,num3;
+    cout<<"enter three number: ";
+    cin>>num1>>num2>>num3;
+    int sum =0;
+    sum = num1 + num2 + num3;
+    
+    cout<<"average of the arraye element is: "<<sum/3<<endl;
+
+    return 0;
+}
