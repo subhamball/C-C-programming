9405b60f12948232d4df990d1fa6d185b6d844e2
Subham Ball <123955835+subhamball@users.noreply.github.com>
2023-12-31 13:07:11 +0530

    Add files via upload

---
+++ length_of_string_using_length.cpp
@@ -0,0 +1,15 @@
+//C++ Program to Find the Length of a String
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+int main(){
+
+    string str;
+    cout<<"enter a string :";
+    cin>>str;
+
+    cout<<str.length()<<endl;
+
+    return 0;
+}
