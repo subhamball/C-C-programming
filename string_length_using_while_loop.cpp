a85bc5bc440619875c4865344f50042f5995e0d6
Subham Ball <123955835+subhamball@users.noreply.github.com>
2023-12-31 13:06:00 +0530

    Add files via upload

---
+++ string_length_using_while_loop.cpp
@@ -0,0 +1,20 @@
+//C++ Program to Find the Length of a String
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+int main(){
+
+    string str;
+    cout<<"enter a string :";
+    cin>>str;
+
+    int i=0;
+
+    while(str[i]){
+        i++;
+    }
+    cout<<i<<endl;
+
+    return 0;
+}
