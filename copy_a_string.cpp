295a11d24a4139474a3e0960719f075aa05d57ba
Subham Ball <123955835+subhamball@users.noreply.github.com>
2024-01-01 15:45:35 +0530

    Add files via upload

---
+++ copy_a_string.cpp
@@ -0,0 +1,17 @@
+//C++ Program to Copy Strings
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+int main(){
+
+    string str1, str2;
+    cout<<"enter first string :"<<endl;
+    cin>>str1;
+
+    str2 = str1;
+    cout<<"copied string is :"<<str2<<endl;
+
+    return 0;
+
+}
