dbac5915a23fecfcf90ff38f39a9eb8118f9890b
Subham Ball <123955835+subhamball@users.noreply.github.com>
2024-01-01 22:40:10 +0530

    Add files via upload

---
+++ explicit_casting_double_to_int.cpp
@@ -0,0 +1,22 @@
+//When the user manually changes data from one type to another, this is known as explicit conversion. 
+//This type of conversion is also known as type casting.
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    double num1;
+    cout<<"enter a integer number: ";
+    cin>>num1;
+
+    int num2;
+
+    //implicit conversion
+    num2 = (double)num1;
+
+    cout<<"num1: "<<num1<<endl;
+    cout<<"num2: "<<num2<<endl;
+
+    return 0;
+
+}
