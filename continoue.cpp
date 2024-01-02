756c483059479d2bdeca221293ae569f8c159e6a
Subham Ball <123955835+subhamball@users.noreply.github.com>
2024-01-02 14:05:20 +0530

    Add files via upload

---
+++ continoue.cpp
@@ -0,0 +1,18 @@
+// program to print the value of i
+//continue skips the current iteration
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (int i = 1; i <= 5; i++) {
+        // condition to continue
+        if (i == 3) {
+            continue;
+        }
+
+        cout << i << endl;
+    }
+
+    return 0;
+}
