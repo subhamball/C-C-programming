cbb916d61541d98557e4fe6770691489c5a5cb85
Subham Ball <123955835+subhamball@users.noreply.github.com>
2024-01-02 14:08:58 +0530

    Add files via upload

---
+++ for_loop.cpp
@@ -0,0 +1,11 @@
+// C++ Program to display a text 5 times
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (int i = 1; i <= 5; ++i) {
+        cout <<  "Subham Ball"<<endl;
+    }
+    return 0;
+}
