b52911dedd40b2f3f07aedd424837f284bedc239
Subham Ball <123955835+subhamball@users.noreply.github.com>
2024-01-02 18:49:16 +0530

    Add files via upload

---
+++ display_a_text_using_funtion.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+//function declaretion and defination
+void display(){
+
+    cout<<"Hello, world "<<endl;
+}
+
+int main(){
+
+    // calling function
+    display();
+
+    return 0;
+}
