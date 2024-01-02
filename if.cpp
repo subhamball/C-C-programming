1b7cd5419747e27e78210ea85555f1460d9b025b
Subham Ball <123955835+subhamball@users.noreply.github.com>
2024-01-02 14:03:57 +0530

    Add files via upload

---
+++ if.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    double num;
+    cout<<"enter a integer number: ";
+    cin>>num;
+
+    if(num>=0){
+        cout<<"positive number"<<endl;
+    }
+
+    return 0;
+
+}
