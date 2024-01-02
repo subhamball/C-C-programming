5bec990df859e137665a1b8b635e807f3baeed3d
Subham Ball <123955835+subhamball@users.noreply.github.com>
2024-01-02 14:05:06 +0530

    Add files via upload

---
+++ break.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int i=1;
+
+    while(i){
+        cout<<i<<endl;
+        i++;
+        if(i==10){
+            break;
+        }
+    }  
+
+    return 0;
+}
