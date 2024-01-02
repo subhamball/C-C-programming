b73fd9ac787db20c6f7ef94da970095b7334618e
Subham Ball <123955835+subhamball@users.noreply.github.com>
2024-01-02 14:04:10 +0530

    Add files via upload

---
+++ if_else.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    double num;
+    cout<<"enter a integer number: ";
+    cin>>num;
+
+    if(num>=0){
+        cout<<"positive number"<<endl;
+    }
+    else{
+        cout<<"negetive number"<<endl;
+    }
+
+    return 0;
+
+}
