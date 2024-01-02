7fc7ef9fef5a4f9ee7d924e079fbe347db926da2
Subham Ball <123955835+subhamball@users.noreply.github.com>
2024-01-02 14:06:09 +0530

    Add files via upload

---
+++ infinite_do_while.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num;
+    cout<<"enter a number: ";
+    cin>> num;
+
+    do{
+        cout<<num<<endl;
+        num++;
+    }while(1);
+
+    return 0;
+}
