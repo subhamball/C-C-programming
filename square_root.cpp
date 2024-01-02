39841c837fbac86b5aade806c8545c9397ca5285
Subham Ball <123955835+subhamball@users.noreply.github.com>
2024-01-02 18:55:51 +0530

    Add files via upload

---
+++ square_root.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main(){
+
+    double number,sq_root;
+    cout<<"enter a number: ";
+    cin>>number;
+
+    sq_root = sqrt(number);
+    cout<<"square root is: "<<sq_root<<endl;
+
+    return 0;
+
+}
