438e0355700a17cd2fc4f5a364ddac7636d5f9a8
Subham Ball <123955835+subhamball@users.noreply.github.com>
2024-01-03 06:53:09 +0530

    Add files via upload

---
+++ structure_and_function.cpp
@@ -0,0 +1,34 @@
+//Structure is a collection of variables of different data types under a single name
+#include<iostream>
+using namespace std;
+
+struct employee{
+    char name[20];
+    int age;
+    float weight;
+};
+
+void display(employee e){
+
+    cout << ".................Displaying Information................." << endl;
+    cout << " Employee Name: " << e.name << endl;
+    cout <<"Employee Age: " << e.age << endl;
+    cout << "Employee weight: " << e.weight<<endl;
+
+}
+
+int main(){
+
+    employee e;
+    
+    cout<<"enter emplyee name: ";
+    cin>>e.name;
+    cout<<"employee age: ";
+    cin>>e.age;
+    cout<<"enter employee weight: ";
+    cin>>e.weight;
+
+    display(e);
+
+    return 0;
+}
