2c80f2a616d87284a24be246a6158162b65c8cda
Subham Ball <123955835+subhamball@users.noreply.github.com>
2024-01-04 11:51:58 +0530

    Add files via upload

---
+++ pass_by_value.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+void swap(int a, int b){
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
+}
+
+int main(){
+
+    int num1=10,num2=20;
+    cout<<"num1 and num2 value before swap: "<<num1<<" "<<num2<<endl;
+
+    swap(num1,num2);
+
+    cout<<"num1 and num2 value after swap: "<<num1<<" "<<num2<<endl;
+
+    return 0;
+}
