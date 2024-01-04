a36c7d111f4273006191df89e5f52c07fb48cbc0
Subham Ball <123955835+subhamball@users.noreply.github.com>
2024-01-04 11:51:20 +0530

    Add files via upload

---
+++ dynamic_memory_allocation.cpp
@@ -0,0 +1,31 @@
+//The new operator allocates memory to a variable
+//Once we no longer need to use a variable that we have declared dynamically, we can deallocate the memory occupied by the variable.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  // declare an int pointer
+  int* pointInt;
+
+  // declare a float pointer
+  float* pointFloat;
+
+  // dynamically allocate memory
+  pointInt = new int;
+  pointFloat = new float;
+
+  // assigning value to the memory
+  *pointInt = 45;
+  *pointFloat = 45.45f;
+
+  cout << *pointInt << endl;
+  cout << *pointFloat << endl;
+
+  // deallocate the memory
+  delete pointInt;
+  delete pointFloat;
+
+  return 0;
+}
